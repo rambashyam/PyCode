afa5a09b7943d834591f81d76ae3e0c6fd2c35d8
Bharath <bram876@gmail.com>
2021-12-11 16:03:37 -0800

    Updating the files

--- rand.py
+++ rand.py
@@ -3,6 +3,7 @@ def calculatePy(a:List[List[int]):
 	for i in range(len(a)):
 		for j in range(len(a[0]):
 			print(a[i][j])
+			print(a[j[i])
 
 # this is some random pycode I wrote for checking out GH
 	

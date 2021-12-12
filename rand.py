a5c3640748b12248d0d51c51cbe030052abf36ae
Bharath <bram876@gmail.com>
2021-12-11 16:06:41 -0800

    Brief change in the python code for rand.py

--- rand.py
+++ rand.py
@@ -4,6 +4,7 @@ def calculatePy(a:List[List[int]):
 		for j in range(len(a[0]):
 			print(a[i][j])
 			print(a[j[i])
+	print("Exiting the for loop")
 
 # this is some random pycode I wrote for checking out GH
 	
